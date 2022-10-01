34d55d3ef56fb8d09802b1a94fdf981894016e25
nathanfolwell <nfolwell@schools.nyc.gov>
2022-10-01 18:13:18 -0400

    Adding stuff

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Why does it come with a Main?\n Hello world, btw.");
+         System.out.println("Why does it come with a Main?\n Hello world, btw.");
     }
 }
